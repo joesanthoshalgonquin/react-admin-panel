0c46fc7e5c50e437cf2cbf302a81b63ca23b9c43
joesanthoshalgonquin <sant0232@algonquinlive.com>
2024-01-31 12:37:46 -0500

    Data provider and live provider for graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import { GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
-
 import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
-
-// import dataProvider, { liveProvider } from "@refinedev/nestjs-query";
 import routerBindings, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import { App as AntdApp } from "antd";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { dataProvider, liveProvider } from "./providers";
 
 function App() {
   return (
@@ -21,8 +20,8 @@ function App() {
         <AntdApp>
           <DevtoolsProvider>
             <Refine
-              // dataProvider={dataProvider(gqlClient)}
-              // liveProvider={liveProvider(wsClient)}
+              dataProvider={dataProvider}
+              liveProvider={liveProvider}
               notificationProvider={useNotificationProvider}
               routerProvider={routerBindings}
               options={{
